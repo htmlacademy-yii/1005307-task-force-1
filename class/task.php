fc1504a24f609735cfd4e05b0328a8ca9de02257
anyakul <anyakulikova111@gmail.com>
2021-02-09 16:15:31 +0300

    задание 3. Правки

--- class/task.php
+++ class/task.php
@@ -1,65 +1,83 @@
 <?php
 
-class Task {
-  
-  const STATUS_NEW = 'НОВЫЙ';
-  const STATUS_WORK = 'В РАБОТЕ';
-  const STATUS_CANCELLED = 'ОТМЕНЕНО';
-  const STATUS_DONE = 'ВЫПОЛНЕНО';
-  const STATUS_FAILED = 'ПРОВАЛЕНО';
-  const ACTION_CANCEL = 'ОТМЕНИТЬ';
-  const ACTION_RESPOND = 'ОТКЛИКНУТЬСЯ';
-  const ACTION_DONE = 'ВЫПОЛНЕНО';
-  const ACTION_REFUSE = 'ОТКАЗАТЬСЯ';
+class Task
+{
 
-  public $idDoer;
-  public $idClient;
-  public $currentStatus;
-  
-  public function __construct($idDoer, $idClient, $currentStatus) {
-    $this->idDoer = $idDoer; 
-    $this->idClient = $idClient;
-    $this->currentStatus = $currentStatus; 
-  }
-  
-  public function getStatusAll() {
-    $statusAll = ['new' => STATUS_NEW,
-                 'work' => STATUS_WORK,
-                 'cancelled' => STATUS_CANCELLED, 'done' => STATUS_DONE,
-                 'failed' => STATUS_FAILED
-                 ];
-    return $statusAll;    
-  }
-  
-  public function getActionsAll() {
-    $actionAll = ['cancel' => ACTION_CANCEL,
-                  'respond' => ACTION_RESPOND,   'done' => ACTION_DONE,
-                  'refuse' => ACTION_REFUSE
-                  ];
-    return $actionAll;
-  }
+    public const STATUS_NEW = "НОВЫЙ";
+    public const STATUS_WORK = 'В РАБОТЕ';
+    public const STATUS_CANCELLED = 'ОТМЕНЕНО';
+    public const STATUS_DONE = 'ВЫПОЛНЕНО';
+    public const STATUS_FAILED = 'ПРОВАЛЕНО';
+    public const ACTION_CANCEL = 'ОТМЕНИТЬ';
+    public const ACTION_RESPOND = 'ОТКЛИКНУТЬСЯ';
+    public const ACTION_DONE = 'ВЫПОЛНЕНО';
+    public const ACTION_REFUSE = 'ОТКАЗАТЬСЯ';
 
-  public function getPosibleStatus($currentStatus) {
-    switch($currentStatus) {
-      case self::STATUS_NEW: return ['work' => STATUS_WORK, 'canceled' => STATUS_CANCELLED];
-      case self::STATUS_WORK: return ['done' => STATUS_DONE, 'failed' => STATUS_FAILED];      
-    } 
-    return null;    
-  }
-  
-  public function getPosibleActionForClient($currentStatus) {   
-    switch($currentStatus) {
-      case self::STATUS_NEW: return ['cancel' => ACTION_CANCEL];
-      case self::STATUS_WORK: return ['done' => ACTION_DONE];
+    public $idDoer;
+    public $idClient;
+    public $currentStatus;
+
+    public function __construct($idDoer, $idClient, $currentStatus)
+    {
+        $this->idDoer        = $idDoer;
+        $this->idClient      = $idClient;
+        $this->currentStatus = $currentStatus;
     }
-    return null;
-  }
-  
-  public function getPosibleActionForDoer($currentStatus) {
-    switch($currentStatus) {
-      case self::STATUS_NEW: return ['respond' => ACTION_RESPOND];
-      case self::STATUS_WORK: return ['refuse' => ACTION_REFUSE];
-      return null; 
+
+    public function getStatusAll(): array
+    {
+        return [
+            'new'       => self::STATUS_NEW,
+            'work'      => self::STATUS_WORK,
+            'cancelled' => self::STATUS_CANCELLED,
+            'done'      => self::STATUS_DONE,
+            'failed'    => self::STATUS_FAILED,
+        ];
     }
-  }
+
+    public function getActionsAll(): array
+    {
+        return [
+            'cancel'  => self::ACTION_CANCEL,
+            'respond' => self::ACTION_RESPOND,
+            'done'    => self::ACTION_DONE,
+            'refuse'  => self::ACTION_REFUSE,
+        ];
+    }
+
+    public function getPossibleStatus($currentStatus): array
+    {
+        switch ($currentStatus) {
+            case self::STATUS_NEW:
+                return ['work' => self::STATUS_WORK, 'canceled' => self::STATUS_CANCELLED];
+            case self::STATUS_WORK:
+                return ['done' => self::STATUS_DONE, 'failed' => self::STATUS_FAILED];
+        }
+
+        return array();
+    }
+
+    public function getPossibleActionForClient($currentStatus): array
+    {
+        switch ($currentStatus) {
+            case self::STATUS_NEW:
+                return ['cancel' => self::ACTION_CANCEL];
+            case self::STATUS_WORK:
+                return ['done' => self::ACTION_DONE];
+        }
+
+        return array();
+    }
+
+    public function getPossibleActionForDoer($currentStatus): array
+    {
+        switch ($currentStatus) {
+            case self::STATUS_NEW:
+                return ['respond' => self::ACTION_RESPOND];
+            case self::STATUS_WORK:
+                return ['refuse' => self::ACTION_REFUSE];
+        }
+
+        return array();
+    }
 }
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-set_include_path('class');
-spl_autoload_register();
+declare(strict_types = 1);
+//set_include_path('class');
+//spl_autoload_register();
+require 'class/task.php';
 
-$idDoer = 1;
-$idClient = 2;
-$currentStatus = Task::STATUS_WORK;
-$task = new Task($idDoer, $idClient, $currentStatus);
-$isTaskStatusAll = $task->getStatusAll();
-$isTaskActionsAll = $task->getActionsAll();
-$isPosibleActionForClient = $task->getPosibleActionForClient($currentStatus);
-$isPosibleActionsForDoer = $task->getPosibleActionForDoer(Task::STATUS_NEW);
-$isPosibleStatus = $task->getPosibleStatus($currentStatus);
-var_dump($isPosibleActionForClient);
-?>
+$idDoer                   = 1;
+$idClient                 = 2;
+$currentStatus            = Task::STATUS_WORK;
+$task                     = new Task($idDoer, $idClient, $currentStatus);
+$isTaskStatusAll          = $task->getStatusAll();
+$isTaskActionsAll         = $task->getActionsAll();
+$isPossibleActionForClient = $task->getPossibleActionForClient($currentStatus);
+$isPossibleActionsForDoer  = $task->getPossibleActionForDoer(Task::STATUS_NEW);
+$isPossibleStatus          = $task->getPossibleStatus($currentStatus);
+var_dump($isPossibleActionForClient);
