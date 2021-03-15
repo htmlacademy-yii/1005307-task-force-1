69e258a04e56291159a1eff44b0f59f64d18eb89
anyakul <anyakulikova111@gmail.com>
2021-03-15 14:55:45 +0300

    редактирую базу данных и создаю схему классов

--- scheme.sql
+++ scheme.sql
@@ -2,44 +2,6 @@ CREATE
 DATABASE IF NOT EXISTS TaskForce DEFAULT CHARACTER SET UTF8 DEFAULT COLLATE UTF8_GENERAL_CI;
 USE
 TaskForce;
-CREATE TABLE IF NOT EXISTS USERS (
-    id                        INT AUTO_INCREMENT PRIMARY KEY,
-    date_registration         DATE,
-    email                     varchar(128),
-    user_password             varchar(128),
-    avatar                    varchar(128),
-    name_user                 varchar(128),
-    city_id                   int(10),
-    birthday                  DATE,
-    description               text,
-    rating                    decimal(3, 2),
-    phone                     int(10),
-    skype                     varchar(128),
-    telegram                  varchar(128),
-    alert_new_message         TINYINT(1),
-    alert_action_task         TINYINT(1),
-    alert_new_comment         TINYINT(1),
-    show_contacts_only_client TINYINT(1),
-    hide_my_account           TINYINT(1)
-);
-
-CREATE TABLE IF NOT EXISTS tasks (
-    id               INT AUTO_INCREMENT PRIMARY KEY,
-    nameTask         varchar(128),
-    id_category      int(10),
-    date_create      DATETIME,
-    price            int(5),
-    description      text,
-    city_id          int(10),
-    latitude         int(3),
-    longitude        int(3),
-    address          varchar(128),
-    location_comment varchar(128),
-    statusTask       varchar(128),
-    doer_id          int(10),
-    client_id        int(10),
-    deadline         DATETIME
-);
 
 CREATE TABLE IF NOT EXISTS categories (
     id              INT AUTO_INCREMENT PRIMARY KEY,
@@ -64,13 +26,6 @@ CREATE TABLE IF NOT EXISTS favourites
     task_id        int(10)
 );
 
-CREATE TABLE IF NOT EXISTS portfolio_photo
-(
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    user_id int(10),
-    photo   varchar(128)
-);
-
 CREATE TABLE IF NOT EXISTS file_task
 (
     id        INT AUTO_INCREMENT PRIMARY KEY,
@@ -88,20 +43,15 @@ CREATE TABLE IF NOT EXISTS messages
     time_create DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS response_task
+CREATE TABLE IF NOT EXISTS notifications
 (
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    task_id       int(10),
-    doer_id       int(10),
-    title         varchar(128),
-    text_response text,
-    time_create   DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS user_category
-(
-    user_id     int(10),
-    category_id int(10)
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id int(10),
+title varchar(128),
+is_view TINYINT(1),
+createTime DATETIME,
+type varchar(128),
+task_id int(10)
 );
 
 CREATE TABLE IF NOT EXISTS opinions
@@ -113,4 +63,68 @@ CREATE TABLE IF NOT EXISTS opinions
     client_id    int(10),
     task_id      int(10),
     time_create  DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS portfolio_photo
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    user_id int(10),
+    photo   varchar(128)
+);
+
+CREATE TABLE IF NOT EXISTS response_task
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    task_id       int(10),
+    doer_id       int(10),
+    title         varchar(128),
+    text_response text,
+    time_create   DATETIME
+);
+
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    nameTask         varchar(128),
+    id_category      int(10),
+    date_create      DATETIME,
+    price            int(5),
+    description      text,
+    city_id          int(10),
+    latitude         int(3),
+    longitude        int(3),
+    address          varchar(128),
+    location_comment varchar(128),
+    statusTask       varchar(128),
+    doer_id          int(10),
+    client_id        int(10),
+    deadline         DATETIME
+);
+
+
+CREATE TABLE IF NOT EXISTS USERS (
+    id                        INT AUTO_INCREMENT PRIMARY KEY,
+    date_registration         DATE,
+    email                     varchar(128),
+    user_password             varchar(128),
+    avatar                    varchar(128),
+    name_user                 varchar(128),
+    city_id                   int(10),
+    birthday                  DATE,
+    description               text,
+    rating                    decimal(3, 2),
+    phone                     int(10),
+    skype                     varchar(128),
+    telegram                  varchar(128),
+    alert_new_message         TINYINT(1),
+    alert_action_task         TINYINT(1),
+    alert_new_comment         TINYINT(1),
+    show_contacts_only_client TINYINT(1),
+    hide_my_account           TINYINT(1)
+);
+
+CREATE TABLE IF NOT EXISTS user_category
+(
+    user_id     int(10),
+    category_id int(10)
 );
