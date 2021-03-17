0ee5baba98605b8b6880c2060c387e9b96097ef1
anyakul <anyakulikova111@gmail.com>
2021-03-17 15:14:32 +0300

    Добавляю внешние ключи на поля бд

--- scheme.sql
+++ scheme.sql
@@ -3,107 +3,23 @@ DATABASE IF NOT EXISTS TaskForce DEFAULT CHARACTER SET UTF8 DEFAULT COLLATE UTF8
 USE
 TaskForce;
 
-CREATE TABLE IF NOT EXISTS categories (
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    name_categories varchar(128),
-    work_online     TINYINT(1)
+CREATE TABLE IF NOT EXISTS categories
+(
+    category_id    INT AUTO_INCREMENT PRIMARY KEY,
+    name_category  varchar(128),
+    eng_name       varchar(128),
+    work_online    TINYINT(1)
 );
 
 CREATE TABLE IF NOT EXISTS cities
 (
-    id        INT AUTO_INCREMENT PRIMARY KEY,
+    city_id   INT AUTO_INCREMENT PRIMARY KEY,
     name_city varchar(128)
 );
 
-CREATE TABLE IF NOT EXISTS favourites
-(
-    id             INT AUTO_INCREMENT PRIMARY KEY,
-    user_id        int(10),
-    title          varchar(128),
-    is_view        TINYINT(1),
-    createTime     DATETIME,
-    type_favourite varchar(128),
-    task_id        int(10)
-);
 
-CREATE TABLE IF NOT EXISTS file_task
-(
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    task_id   int(10),
-    file_item varchar(128)
-);
-
-CREATE TABLE IF NOT EXISTS messages
-(
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    text        text,
-    doer_id     int(10),
-    client_id   int(10),
-    task_id     int(10),
-    time_create DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS notifications
-(
-id INT AUTO_INCREMENT PRIMARY KEY,
-user_id int(10),
-title varchar(128),
-is_view TINYINT(1),
-createTime DATETIME,
-type varchar(128),
-task_id int(10)
-);
-
-CREATE TABLE IF NOT EXISTS opinions
-(
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    title        varchar(128),
-    text_opinion text,
-    doer_id      int(10),
-    client_id    int(10),
-    task_id      int(10),
-    time_create  DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS portfolio_photo
-(
-    id      INT AUTO_INCREMENT PRIMARY KEY,
-    user_id int(10),
-    photo   varchar(128)
-);
-
-CREATE TABLE IF NOT EXISTS response_task
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    task_id       int(10),
-    doer_id       int(10),
-    title         varchar(128),
-    text_response text,
-    time_create   DATETIME
-);
-
-
-CREATE TABLE IF NOT EXISTS tasks (
-    id               INT AUTO_INCREMENT PRIMARY KEY,
-    name_task         varchar(128),
-    id_category      int(10),
-    date_create      DATETIME,
-    price            int(5),
-    description      text,
-    city_id          int(10),
-    latitude         int(3),
-    longitude        int(3),
-    address          varchar(128),
-    location_comment varchar(128),
-    statusTask       varchar(128),
-    doer_id          int(10),
-    client_id        int(10),
-    deadline         DATETIME
-);
-
-
-CREATE TABLE IF NOT EXISTS USERS (
-    id                        INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    user_id                   INT AUTO_INCREMENT PRIMARY KEY,
     date_registration         DATE,
     email                     varchar(128),
     user_password             varchar(128),
@@ -120,11 +36,122 @@ CREATE TABLE IF NOT EXISTS USERS (
     alert_action_task         TINYINT(1),
     alert_new_comment         TINYINT(1),
     show_contacts_only_client TINYINT(1),
-    hide_my_account           TINYINT(1)
+    hide_my_account           TINYINT(1),
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
 
 CREATE TABLE IF NOT EXISTS user_category
 (
-    user_id     int(10),
-    category_id int(10)
+    user_category_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id          int(10),
+    category_id      int(10),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    task_id          INT AUTO_INCREMENT PRIMARY KEY,
+    name_task        varchar(128),
+    category_id      int(10),
+    date_create      DATETIME,
+    price            int(5),
+    description      text,
+    city_id          int(10),
+    latitude         int(3),
+    longitude        int(3),
+    address          varchar(128),
+    location_comment varchar(128),
+    statusTask       varchar(128),
+    doer_id          int(10),
+    client_id        int(10),
+    deadline         DATETIME,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (city_id) REFERENCES cities(city_id),
+    FOREIGN KEY (doer_id) REFERENCES users(user_id),
+    FOREIGN KEY (client_id) REFERENCES users(user_id)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS favourites
+(
+    favourite_id   INT AUTO_INCREMENT PRIMARY KEY,
+    user_id        int(10),
+    title          varchar(128),
+    is_view        TINYINT(1),
+    createTime     DATETIME,
+    type_favourite varchar(128),
+    task_id        int(10),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS file_task
+(
+    file_task_id  INT AUTO_INCREMENT PRIMARY KEY,
+    task_id       int(10),
+    file_item     varchar(128),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    messages_id  INT AUTO_INCREMENT PRIMARY KEY,
+    text         text,
+    doer_id      int(10),
+    client_id    int(10),
+    task_id      int(10),
+    time_create  DATETIME,
+    FOREIGN KEY (doer_id) REFERENCES users(user_id),
+    FOREIGN KEY (client_id) REFERENCES users(user_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+);
+
+CREATE TABLE IF NOT EXISTS notifications
+(
+    notifications_id  INT AUTO_INCREMENT PRIMARY KEY,
+    user_id           int(10),
+    title             varchar(128),
+    is_view           TINYINT(1),
+    createTime        DATETIME,
+    type              varchar(128),
+    task_id           int(10),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS opinions
+(
+    opinions_id  INT AUTO_INCREMENT PRIMARY KEY,
+    title        varchar(128),
+    text_opinion text,
+    doer_id      int(10),
+    client_id    int(10),
+    task_id      int(10),
+    time_create  DATETIME,
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (doer_id) REFERENCES users(user_id),
+    FOREIGN KEY (client_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS portfolio_photo
+(
+    portfolio_photo_id  INT AUTO_INCREMENT PRIMARY KEY,
+    user_id             int(10),
+    photo               varchar(128),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS response_task
+(
+    response_task_id  INT AUTO_INCREMENT PRIMARY KEY,
+    task_id           int(10),
+    doer_id           int(10),
+    title             varchar(128),
+    text_response     text,
+    time_create       DATETIME,
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (doer_id) REFERENCES users(user_id)
 );
