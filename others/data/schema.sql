d1f74cd587584dceef9dec6f59875c2b25589d69
anyakul <anyakulikova111@gmail.com>
2021-05-03 14:47:06 +0300

    делаю миграции и корректирую бд

--- others/data/schema.sql
+++ others/data/schema.sql
@@ -5,153 +5,175 @@ TaskForce;
 
 CREATE TABLE IF NOT EXISTS `categories`
 (
-    `id`    INT AUTO_INCREMENT PRIMARY KEY,
-    `name`  varchar(128),
-    `icon`  varchar(128)
-);
+    `id`          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`        varchar(128) NOT NULL unique,
+    `icon`        varchar(128) NOT NULL unique,
+    `profession`  varchar(128) NOT NULL unique
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `cities`
 (
-    `id`    INT AUTO_INCREMENT PRIMARY KEY,
-    `city`  varchar(128),
-    `lat`   DECIMAL(8, 6) DEFAULT NULL,
-    `long`  DECIMAL(8, 6) DEFAULT NULL
-);
+    `id`    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `city`  varchar(128) NOT NULL unique,
+    `lat`   float(8, 6) NOT NULL unique,
+    `long`  float(8, 6) NOT NULL unique
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `id`        INT AUTO_INCREMENT PRIMARY KEY,
-    `email`     varchar(128),
-    `name`      varchar(128),
-    `password`  varchar(128),
-    `dt_add`    DATE
-);
+    `id`        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email`     varchar(128) NOT NULL unique,
+    `name`      varchar(128) NOT NULL unique,
+    `password`  varchar(128) NOT NULL unique,
+    `dt_add`    timestamp DEFAULT CURRENT_TIMESTAMP() NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `profiles` (
-    `id`        INT AUTO_INCREMENT PRIMARY KEY,
+    `id`        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `address`   varchar(128),
     `bd`        DATE,
     `about`     text,
     `phone`     int(10),
     `skype`     varchar(128),
     `telegram`  varchar(128),
+    `avatar`    varchar(128),
+    `rate`      float(3, 2) NOT NULL,
     `user_id`   int(10),
     `city_id`   int(10),
-    `avatar`    varchar(128),
-    `rating`    decimal(3, 2),
-    FOREIGN KEY (`city_id`) REFERENCES cities(`id`),
-    FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-);
+    KEY `city_id` (`city_id`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `cities`(`id`),
+    CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `user_category`
 (
-    `id`           INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id`      int(10),
-    `category_id`  int(10),
-    FOREIGN KEY (`category_id`) REFERENCES categories(`id`),
-    FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-);
+    `id`           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id`      int(10) NOT NULL,
+    `category_id`  int(10) NOT NULL,
+    KEY `city_id` (`category_id`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `user_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES categories(`id`),
+    CONSTRAINT `user_category_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE IF NOT EXISTS `status_task` (
+    `id`    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`  varchar(128) NOT NULL unique
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `tasks` (
-    `id`                INT AUTO_INCREMENT PRIMARY KEY,
-    `dt_add`            DATETIME,
+    `id`                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `dt_add`            timestamp DEFAULT current_timestamp() NOT NULL,
     `category_id`       int(10),
-    `description`       text,
+    `description`       text NOT NULL,
     `expire`            DATETIME,
-    `name`              varchar(128),
-    `city_id`           int(10),
+    `name`              varchar(128) NOT NULL,
     `address`           varchar(128),
     `budget`            int(5),
-    `lat`               decimal(3, 2),
-    `long`              decimal(3, 2),
+    `lat`               float(3, 2),
+    `long`              float(3, 2),
     `location_comment`  varchar(128),
+    `city_id`           int(10),
     `doer_id`           int(10),
-    `client_id`         int(10),
-    `statusTask`        varchar(128),
-    FOREIGN KEY (`category_id`) REFERENCES categories(`id`),
-    FOREIGN KEY (`city_id`) REFERENCES cities(`id`),
-    FOREIGN KEY (`doer_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`client_id`) REFERENCES users(`id`)
-);
+    `client_id`         int(10) NOT NULL,
+    `status_task_id`    int(10) NOT NULL,
+    KEY `category_id` (`category_id`),
+    KEY `city_id` (`city_id`),
+    KEY `doer_id` (`doer_id`),
+    KEY `client_id` (`client_id`),
+    CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES categories(`id`),
+    CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES cities(`id`),
+    CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`doer_id`) REFERENCES users(`id`),
+    CONSTRAINT `tasks_ibfk_4` FOREIGN KEY (`client_id`) REFERENCES users(`id`),
+    CONSTRAINT `tasks_ibfk_5` FOREIGN KEY (`status_task_id`) REFERENCES status_task(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `favourites`
 (
-    `id`              INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id`         int(10),
-    `title`           varchar(128),
-    `is_view`         TINYINT(1),
-    `dt_add`          DATETIME,
-    `type_favourite`  varchar(128),
-    `task_id`         int(10),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`),
-    FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-);
+    `id`                   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title`                varchar(128) NOT NULL,
+    `dt_add`               timestamp DEFAULT current_timestamp() NOT NULL,
+    `type_favourite`       varchar(128),
+    `user_id`              int(10) NOT NULL,
+    `favourite_person_id`  int(10) NOT NULL,
+    KEY `user_id` (`user_id`),
+    KEY `favourite_person_id` (`favourite_person_id`),
+    CONSTRAINT `favourites_ibfk_1` FOREIGN KEY (`favourite_person_id`) REFERENCES users(`id`),
+    CONSTRAINT `favourites_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `file_task`
 (
-    `id`         INT AUTO_INCREMENT PRIMARY KEY,
-    `task_id`    int(10),
-    `file_item`  varchar(128),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
-);
+    `id`         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `file_item`  varchar(128) NOT NULL,
+    `task_id`    int(10) NOT NULL,
+    KEY `task_id` (`task_id`),
+    CONSTRAINT `file_task_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `messages`
 (
-    `id`         INT AUTO_INCREMENT PRIMARY KEY,
-    `doer_id`    int(10),
-    `client_id`  int(10),
-    `text`       text,
-    `task_id`    int(10),
-    `dt_add`     DATETIME,
-    FOREIGN KEY (`doer_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`client_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
-);
+    `id`         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `text`       text NOT NULL,
+    `dt_add`     timestamp DEFAULT CURRENT_TIMESTAMP() NOT NULL,
+    `writer_id`  int(10) NOT NULL,
+    `task_id`    int(10) NOT NULL,
+    KEY `writer` (`writer_id`),
+    KEY `task_id` (`task_id`),
+    CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`writer_id`) REFERENCES users(`id`),
+    CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `notifications`
 (
-    `id`       INT  AUTO_INCREMENT PRIMARY KEY,
-    `user_id`  int(10),
-    `task_id`  int(10),
-    `title`    varchar(128),
+    `id`       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title`    varchar(128) NOT NULL,
     `is_view`  TINYINT(1),
-    `dt_add`   DATETIME,
-    `TYPE`     varchar(128),
-    FOREIGN KEY (`user_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
-);
+    `dt_add`   timestamp DEFAULT current_timestamp() NOT NULL,
+    `type`     varchar(128) NOT NULL,
+    `user_id`  int(10) NOT NULL,
+    `task_id`  int(10) NOT NULL,
+    KEY `user_id` (`user_id`),
+    KEY `task_id` (`task_id`),
+    CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES users(`id`),
+    CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES tasks(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `opinions`
 (
-    `id`           INT AUTO_INCREMENT PRIMARY KEY,
-    `dt_add`       DATETIME,
-    `title`        varchar(128),
-    `description`  text,
+    `id`           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `dt_add`       timestamp NOT NULL DEFAULT current_timestamp() NOT NULL,
+    `title`        varchar(128) NOT NULL,
+    `description`  text NOT NULL,
     `rate`         decimal(3, 2),
-    `doer_id`      int(10),
-    `client_id`    int(10),
-    `task_id`      int(10),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`),
-    FOREIGN KEY (`doer_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`client_id`) REFERENCES users(`id`)
-);
+    `writer_id`    int(10) NOT NULL,
+    `task_id`      int(10) NOT NULL,
+    KEY `writer_id` (`writer_id`),
+    KEY `task_id` (`task_id`),
+    CONSTRAINT `opinions_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES tasks(`id`),
+    CONSTRAINT `opinions_ibfk_2` FOREIGN KEY (`writer_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `portfolio_photo`
 (
-    `id`       INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id`  int(10),
-    `photo`    varchar(128),
-    FOREIGN KEY (`user_id`) REFERENCES users(`id`)
-);
+    `id`       INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `photo`    varchar(128) NOT NULL,
+    `user_id`  int(10) NOT NULL,
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `portfolio_photo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS `replies`
 (
-    `id`           INT AUTO_INCREMENT PRIMARY KEY,
-    `dt_add`       DATETIME,
-    `rate`         decimal(3, 2),
-    `description`  text,
-    `task_id`      int(10),
-    `doer_id`      int(10),
-    `title`        varchar(128),
-    FOREIGN KEY (`task_id`) REFERENCES tasks(`id`),
-    FOREIGN KEY (`doer_id`) REFERENCES users(`id`)
-);
+    `id`           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `dt_add`       timestamp NOT NULL DEFAULT current_timestamp() NOT NULL,
+    `rate`         float(3, 2),
+    `description`  text NOT NULL,
+    `title`        varchar(128) NOT NULL,
+    `task_id`      int(10) NOT NULL,
+    `doer_id`      int(10) NOT NULL,
+    KEY `task_id` (`task_id`),
+    KEY `doer_id` (`doer_id`),
+    CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES tasks(`id`),
+    CONSTRAINT `replies_ibfk_2taskforce` FOREIGN KEY (`doer_id`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
