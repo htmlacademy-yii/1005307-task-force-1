f330c0981256fd7c8f0da38246109cde02edfeb6
anyakul <anyakulikova111@gmail.com>
2021-06-24 01:30:07 +0300

    рендерю страницу регистрации

---
+++ frontend/controllers/SignController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\models\users\UserSearchForm;
+use app\models\users\Users;
+use yii\web\NotFoundHttpException;
+
+class SignController extends Controller
+{
+    public $footerContent = '/sign/footer';
+    public function actionIndex(): string
+    {
+        $searchForm = new UserSearchForm();
+        $searchForm->load($this->request->post());
+        $users = Users::getDoersByFilters($searchForm);
+
+        return $this->render('index', ['users' => $users, 'searchForm' => $searchForm]);
+    }
+}
